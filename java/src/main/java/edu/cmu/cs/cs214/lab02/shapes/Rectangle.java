71f832c9a3b13d6195cfe99c0fb796eacb4bb5e5
rutone <zaga8037@gmail.com>
2025-02-03 23:55:00 +0800

    lab 2 code zasvarlav

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
@@ -1,10 +1,11 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Rectangle implements Shape {
-    public double height;
-    public double width;
-    
-    public Rectangle(double height, double width){
+    // 🔹 Mark fields as final to prevent modification after initialization
+    private final double height;
+    private final double width;
+
+    public Rectangle(double height, double width) {
         this.height = height;
         this.width = width;
     }
